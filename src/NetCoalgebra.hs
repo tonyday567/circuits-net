47bf5f01e219a3575fb910701033fb89b331dae0
Tony Day <tonyday567@gmail.com>
2026-09-06 17:17:48 +1000

    adapt to circuits refactor: Circuit.Moore -> Circuit.Machine (Coalgebra)

--- src/NetCoalgebra.hs
+++ src/NetCoalgebra.hs
@@ -50,7 +50,7 @@ where
 import Circuit.Diff.Param (DiffP (..), runDiffP)
 import Circuit.Learn.Ephemeral (Progress (..), sgd)
 import Circuit.Mat.Dense (Matrix (..), matTimes, matVec)
-import Circuit.Moore (Coalgebra (..))
+import Circuit.Machine (Coalgebra (..))
 import Circuit.Poly (Eval (..), Mono, lens)
 import Data.Vector.Unboxed qualified as VU
 import Harpie.Array (Array, arrayAs)
